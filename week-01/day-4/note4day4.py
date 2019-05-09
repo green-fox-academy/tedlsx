5141235946d6ab84d29f59c308f6a8b5a43b1ba8
tedlsx <ted464228100@gmail.com>
2019-05-09 13:17:54 +0800

    solution for day4 re

---
+++ week-01/day-4/note4day4.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  9 09:18:24 2019
+
+@author: Ted_Liu
+"""
+
---
+++ week-01/day-4/solution4day4.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  9 09:46:15 2019
+
+@author: Ted_Liu
+"""
+
