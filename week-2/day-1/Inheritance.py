a352f06fa04b718de99e9de9b22385f5c5371164
tedlsx <ted464228100@gmail.com>
2019-05-13 20:43:48 +0800

    add inheritance

--- week-2/day-1/Inheritance.py
+++ week-2/day-1/Inheritance.py
@@ -25,16 +25,22 @@ p1.get_goal()
 ### Student
 
 class Student(Person):
-    def __init__(self, previous_organization, skipped_days):
+    def __init__(self, name, age, gender, previous_organization, skipped_days):
+        Person.__init_(self, name, age, gender)
+            
         self.previous_organization = previous_organization
         self.skipped_days = skipped_days
         
     def get_goal(self):
         print("Be a junior software developer.")
         
-    def intrduce(self):
+    def introduce(self):
+        print("Hi, I'm ", self.name, " a ", self.age, " year old ", self.gender, " from ", self.previous_organization, " who skipped ", self.skipped_days, " days from the course already")
         
+    def skipped_days(number_of_days):
+        self.skipped_days += self.num_of_days
         
+    
         
         
         
