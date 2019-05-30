1d9439c70f85a8011b59ad832141090c21da10a3
tedlsx <ted464228100@gmail.com>
2019-05-30 10:40:49 +0800

    sql for diff file types

--- week-4/day-3/employee.py
+++ week-4/day-3/employee.py
@@ -13,28 +13,57 @@ con = psy.connect(
 # print(con.get_dsn_parameters())
 cur = con.cursor()
 cur.execute("""
-CREATE TABLE if not exists employee(
+CREATE TABLE if not exists employee1(
 id integer,
-first_name varchar(20),
-last_name varchar(10),
-birth_date int,
-nationality varchar(10),
-gender varchar(10),
+first_name varchar(100),
+last_name varchar(100),
+birth_date date,
+gender varchar(100),
+monthly_salary int)
+""")
+
+cur.execute("""
+CREATE TABLE if not exists employee2(
+id integer,
+name varchar(100),
+birth_date date,
+nationality varchar(100),
+gender varchar(100),
 monthly_salary int,
-university varchar(50))
+university varchar(100))
+""")
+
+cur.execute("""
+CREATE TABLE if not exists employee3(
+id integer,
+name varchar(100),
+birth_date date,
+branch varchar(100),
+gender varchar(100),
+salary_by_year int,
+position varchar(100))
 """)
 
 def employee_create():
     e1 = open("employees.csv", "r")
-    e2 = open("employee.json", "r")    
-    tree = ET.parse("emplyee.xml")
-    root = tree.getroot()
+    e2 = open("employees.json", "r")    
+    # tree = ET.parse("employees.xml")
+    # root = tree.getroot()
 
     my_list = []
 
     data_csv = csv.reader(e1, delimiter=",")
-    for row in data_csv:
-        my_list.append(row)
+    my_list = list(data_csv)[1:]
+    for row in my_list:
+        cur.execute("insert into employee1 values (%s, %s, %s, %s, %s, %s)", row)
+        con.commit()
+
     data_json = json.load(e2)
     for line in data_json:
-            my_list.append(line)
+        cur.execute("insert into employee2 values (%s, %s, %s, %s, %s, %s, %s)", [line["id"], line["name"], line["birth_date"], line["nationality"], line["gender"], line["monthly_salary"], line["university"]])
+        con.commit()
+
+    # for elem in root:
+    #     for subelem in elem:
+    #         my_list.append(subelem.attrib.get())
+    
