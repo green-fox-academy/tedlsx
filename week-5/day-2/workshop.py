dc6258835633a9e365390d9c68d343cdb7d6a602
tedlsx <ted464228100@gmail.com>
2019-06-05 09:01:08 +0800

    upload first question solution for workshop day-2 for nosql

---
+++ week-5/day-2/workshop.py
@@ -0,0 +1,11 @@
+#Where is this weather station
+select country_code from weather_station where id = '724940:23234';
+
+#Show the temperature for June 1st, 2008.
+
+
+# Show the temperatures for June 1st, 2008 from 9AM to 3PM.
+# What is the average elevation of the weather stations in Indiana?
+# What is the latitude of the northest weather station in Texas?
+# Insert the current temperature for today.
+# Fill the daily_aggregate_temperature table.
